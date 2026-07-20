74e3a68aba44bea52ed1bae563c79595522e9216
MrTheGoat <edifyknp.tanushgr2c076@gmail.com>
2026-07-20 01:03:23 +0000

    Meow

--- secretclub.py
+++ secretclub.py
@@ -1,3 +1,5 @@
+from colorama import Fore, Back, Style
+
 strength = {
     'name': 10,
     'surname': 20,
@@ -23,16 +25,16 @@ def short_form(idx, *names, **addons):
                 final = final + addons[key]
 
             else:
-                print("You are not good at typing, are you ...")
+                print("You are not good at typing, are you ..."+Fore.RED)
 
     except TypeError as e:
-        print("Wrong data type:", e)
+        print("Wrong data type:"+Fore.RED, e)
 
     except IndexError as e:
-        print("Indexing is going wrong:", e)
+        print("Indexing is going wrong:"+Fore.RED, e)
 
     except Exception as e:
-        print("Something went wrong:", e)
+        print("Something went wrong:"+Fore.RED, e)
 
     else:
         return final.upper()
@@ -50,7 +52,7 @@ def name_strength(**fullname):
             result += strength[key] * len(value)
 
     except KeyError as e:
-        print("Perhaps you are giving too many fields:", e)
+        print("Perhaps you are giving too many fields:"+Fore.RED, e+Fore.RED)
 
     else:
         return result
@@ -63,32 +65,32 @@ def validate(**prelimdata):
         assert prelimdata["ns"] != None
 
     except KeyError as e:
-        print("You are really bad at typing, aren't you?")
-        print("Validation failed due to", e)
+        print("You are really bad at typing, aren't you?"+Fore.RED)
+        print("Validation failed due to"+Fore.RED, e+Fore.RED)
 
     except AssertionError:
-        print("One of the earlier steps has gone wrong.")
-        print("Validation failed")
+        print("One of the earlier steps has gone wrong."+Fore.RED)
+        print("Validation failed"+Fore.RED)
 
     else:
 
-        print("Looking good so far ...")
-        print("Sending for deeper validation")
-        print("Opening connection to a remote server ...")
+        print("Looking good so far ..."+ Fore.RED)
+        print("Sending for deeper validation"+Fore.RED)
+        print("Opening connection to a remote server ..."+Fore.RED)
 
         try:
             deep_validate()
 
         except ValueError:
-            print("There is something deeply wrong")
-            print("Validation failed ...")
+            print("There is something deeply wrong"+Fore.RED)
+            print("Validation failed ..."+Fore.RED)
 
         else:
-            print("Congrats ... Validation Successful")
-            print("Welcome to my world!")
+            print("Congrats ... Validation Successful"+Fore.GREEN)
+            print("Welcome to my world!"+Fore.BLUE)
 
         finally:
-            print("Closing connection to the remote server")
+            print("Closing connection to the remote server"+Fore.WHITE)
 
 
 def deep_validate():
@@ -99,9 +101,9 @@ def deep_validate():
     pass
 
 
-print("Welcome to the comrades' club ","\n","Here we will teach you how to fight with the elements, shall you recieve a blessing from the archons of the god king",'Start you blessing validation')
-print(' us these functions to start your blessing','\n'
+print(Back.WHITE+Style.DIM+"Welcome to the comrades' club ","\n","Here we will teach you how to fight with the elements, shall you recieve a blessing from the Archons of the God King",'Start your blessing validation')
+print(Back.RESET+Fore.RED+Style.BRIGHT+' use these functions to start your blessing','\n'
       ,'short_form(names and adjectives about urself)','\n'
       ,'name_strength(your full name with addons and prefix with suffix)',"\n"
-      ,'validate(short_form and name_strength)','\n'
+      ,'validate(short_form and name_strength()','\n'
       ,'then deep_validate()')
--- something.py
+++ something.py
@@ -6,12 +6,12 @@ def square(x,y,l):
     t.goto(x,y)
     t.pendown()
     t.goto(x+l,y)
-    t.goto(x+1,y+l)
-    t.goto(x,y+1)
+    t.goto(x+l,y+l)
+    t.goto(x,y+l)
     t.goto(x,y)
 
     if l>10:
-        square(x+l,                            y+l,l/2)
+        square(x+l,y+l,l/2)
 
 square(0,0,100)
 turtle.mainloop()
